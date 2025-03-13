80db2f96b02832a0d4343feaf8e802f280354e7d
Zakria Islam <anonymsjit7861@gmail.com>
2025-03-14 03:04:43 +0500

    Counter Detection for Interaction
    - Added a raycast to detect if player is close to and facing a counter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,51 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float _moveSpeed = 7f;
     [SerializeField] private float _rotationSpeed = 10f;
+    [SerializeField] private float _interactDist = 2f; //the max dist for interaction raycast
 
     [SerializeField] private GameInput gameInput;
+
     public bool IsWalking { get; private set; }
 
     private float _playerHeight = 2f;
     private float _playerRadius = 0.7f;
-    
-    private float _moveDist;
-    
-    private bool _canMove;
-
+    private float _moveDist; //how much distance the player will move in a frame 
+    private bool _canMove; //can the player move or not
+    private Vector3 _lastInteractDir; //the latest move dir before moveDir becoms 0 (player stops pressing any input buttons)
 
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+
+    private void HandleInteractions()
+    {
+        //get the normalized player input direction vectosr (2D movement)
+        Vector2 inputDir = gameInput.GetMovementVectorNormalized();
+
+        //cast 2D input to 3D vector for world movement
+        Vector3 moveDir = new(inputDir.x, 0f, inputDir.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            _lastInteractDir = moveDir;
+        }
+
+        //do a raycast to check whether the player can interact with something or not
+        if (Physics.Raycast(transform.position, _lastInteractDir, out RaycastHit raycastHit,_interactDist))
+        {
+            //player can interact with something
+        }
+        else
+        {
+            //player is too far away and cannot interact with anything
+        }
+    }
+
+
+    private void HandleMovement()
     {
         //get the normalized player input direction vectosr (2D movement)
         Vector2 inputDir = gameInput.GetMovementVectorNormalized();
@@ -32,7 +64,7 @@ public class Player : MonoBehaviour
 
         //do a raycast for collision detection
         _canMove = !Physics.CapsuleCast(transform.position, transform.position + (Vector3.up * _playerHeight), _playerRadius, moveDir, _moveDist);
-        
+
         if (!_canMove)
         {
             //if cannot move (diagonal capsule cast)
@@ -48,7 +80,7 @@ public class Player : MonoBehaviour
                 //move in x direction only (wall hug)
                 moveDir = moveDirX;
             }
-            else 
+            else
             {
                 //attempt only z movement
                 Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized; //account for diagonal movement error
@@ -81,4 +113,5 @@ public class Player : MonoBehaviour
         //rotate player to face direction of movement
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * _rotationSpeed);
     }
+
 }
