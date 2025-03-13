f7861c2d20b8e7fd14b85e2790bafc33e3a10bea
Zakria Islam <anonymsjit7861@gmail.com>
2025-03-13 05:34:31 +0500

    Player Movement
    Added a basic player movement script that uses legacy input system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed;
+
+
+    private void Update()
+    {
+        //to store player input (2D)
+        Vector2 inputDir = Vector2.zero;
+
+        //get player input
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputDir.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputDir.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputDir.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputDir.x = 1;
+        }
+
+        //normalize to account for diagonal movement 
+        inputDir = inputDir.normalized;
+
+        //cast 2D input to 3D vector for world movement
+        Vector3 moveDir = new(inputDir.x, 0f, inputDir.y);
+
+        //move player
+        transform.position += Time.deltaTime * moveSpeed * moveDir;
+
+    }
+}
