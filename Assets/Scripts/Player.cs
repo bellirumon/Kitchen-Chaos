ce58a5bda4010af500e28b1d22b8e4e3a2a10983
Zakria Islam <anonymsjit7861@gmail.com>
2025-03-13 08:23:33 +0500

    Basic Collision Detection
    - Added a basic collision detection system that uses capsule cast to determine whether the player can move or not

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,22 +9,38 @@ public class Player : MonoBehaviour
     [SerializeField] private float _rotationSpeed = 10f;
 
     [SerializeField] private GameInput gameInput;
-
     public bool IsWalking { get; private set; }
+
+    private float _playerHeight = 2f;
+    private float _playerRadius = 0.7f;
+    
+    private float _moveDist;
+    
+    private bool _canMove;
 
 
     private void Update()
     {
-        //get the normalized player input direction vector (2D movement)
+        //get the normalized player input direction vectosr (2D movement)
         Vector2 inputDir = gameInput.GetMovementVectorNormalized();
 
         //cast 2D input to 3D vector for world movement
         Vector3 moveDir = new(inputDir.x, 0f, inputDir.y);
 
-        IsWalking = (moveDir != Vector3.zero);
+        //compute the distance to be moved
+        _moveDist = Time.deltaTime * _moveSpeed;
 
-        //move player
-        transform.position += Time.deltaTime * _moveSpeed * moveDir;
+        //do a raycast for collision detection
+        _canMove = !Physics.CapsuleCast(transform.position, transform.position + (Vector3.up * _playerHeight), _playerRadius, moveDir, _moveDist);
+        
+        //move player if possible
+        if (_canMove)
+        {
+            transform.position += _moveDist * moveDir;
+        }
+
+        //update player walking state
+        IsWalking = (moveDir != Vector3.zero);
 
         //rotate player to face direction of movement
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * _rotationSpeed);
