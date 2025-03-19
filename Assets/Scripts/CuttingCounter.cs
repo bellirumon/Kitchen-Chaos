5c5caeef6eea47b152e91eebaf0b42bbe779d3be
bellirumon <anonymsjit7861@gmail.com>
2025-03-19 11:26:03 +0500

    Completed Cutting Counter Logic
    - Completed the logic for cutting counter
    - Enforced restrictions to only allow cutting on kitchen objects that are cuttable
    - Added cutting recipe scriptable objects, sliced kitchen object scriptable objects and sliced kitchen object prefabs

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class CuttingCounter : BaseCounter
 {
 
-    [SerializeField] private KitchenObjectSO _cutKitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] _cuttingRecipeSO;
 
     public override void Interact(Player player)
     {
@@ -14,10 +15,20 @@ public class CuttingCounter : BaseCounter
             //counter has no kitchen object on it
             if (player.HasKitchenObject())
             {
-                //player is carrying an object
+                //player is carrying a kitchen object
+                if (HasRecipeWithInput(player.GetKitchenObject().KitchenObjectSO))
+                {
+                    //there is a recipe for the kitchen object being carried by the player, hence object is dropable
 
-                //transfer the object from the player to this counter
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                    //transfer the object from the player to this counter
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
+                else
+                {
+                    //object does not have a cutting recipe, do not allow this object to be dropped on this counter
+
+                }
+                
             }
             else
             {
@@ -44,13 +55,43 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractAlternate(Player player)
     {
-        if (HasKitchenObject())
+        if (HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().KitchenObjectSO))
         {
-            //counter has a kitchen object, so cut it (destroy kitchen object and spawn in sliced kitchen object)
+            //counter has a kitchen object AND it has a cutting recipe, so cut it (destroy kitchen object and spawn in sliced kitchen object)
+            KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().KitchenObjectSO);
+
             GetKitchenObject().DestroySelf();
 
-            KitchenObject.SpawnKitchenObject(_cutKitchenObjectSO, this);
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
         }
+    }
+
+
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO cuttingRecipeSO in _cuttingRecipeSO)
+        {
+            if (inputKitchenObjectSO == cuttingRecipeSO.Input)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO input)
+    {
+        foreach (CuttingRecipeSO cuttingRecipeSO in _cuttingRecipeSO)
+        {
+            if (input == cuttingRecipeSO.Input)
+            {
+                return cuttingRecipeSO.Output;
+            }
+        }
+
+        return null;
     }
 
 }
---
+++ Assets/Scripts/CuttingRecipeSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCuttingRecipe", menuName = "ScriptableObjects/CuttingRecipeSO")]
+public class CuttingRecipeSO : ScriptableObject
+{
+    [SerializeField] private KitchenObjectSO _input;
+    public KitchenObjectSO Input => _input;
+
+    [SerializeField] private KitchenObjectSO _output;
+    public KitchenObjectSO Output => _output;
+}
