260dc4c401b37d9637ca094c64061bfcc9d81d13
Zakria Islam <anonymsjit7861@gmail.com>
2025-07-31 01:00:17 +0500

    Implemented Plates Counter
    - Implemented Plates Counter & all related functionality

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    public event Action OnPlateSpawned;
+    public event Action OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectSO _plateKitchenObjectSO;
+
+    private float _spawnPlateTimer;
+    private float _spawnPlateTimerMax = 4f;
+    private int _platesSpawnedAmount;
+    private int _platesSpawnedAmountMax = 4;
+
+
+    private void Update()
+    {
+        _spawnPlateTimer += Time.deltaTime;
+
+        if (_spawnPlateTimer > _spawnPlateTimerMax)
+        {
+            _spawnPlateTimer = 0f;
+
+            if (_platesSpawnedAmount < _platesSpawnedAmountMax)
+            {
+                _platesSpawnedAmount++;
+
+                OnPlateSpawned?.Invoke();
+            }
+        }
+    }
+
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            //player is empty handed 
+            if (_platesSpawnedAmount > 0)
+            {
+                //there is at least one plate on the counter
+
+                _platesSpawnedAmount--;
+
+                KitchenObject.SpawnKitchenObject(_plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke();
+            }
+        }
+    }
+
+}
